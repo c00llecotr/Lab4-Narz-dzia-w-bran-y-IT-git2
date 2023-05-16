10c3929fa8128891be84b47289103f7c2f76a206
Adam Jaskulski <adjask@wp.pl>
2023-05-16 12:44:50 +0200

    Dodanie pliku help.sh

---
+++ help.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo "Dostępne opcje:"
+echo "--date    Wyświetla dzisiejszą datę."
+echo "--logs    Tworzy 100 plików logx.txt i zapisuje w każdym pliku nazwę pliku, nazwę skryptu oraz datę utworzenia."
+echo "--logs 30  Tworzy x plików logx.txt i zapisuje w każdym pliku nazwę pliku, nazwę skryptu oraz datę utworzenia."
+echo "--help    Wyświetla dostępne opcje."
